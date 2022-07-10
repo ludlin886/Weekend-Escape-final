22bd6dc91da9c3ccde2407737500d4ef8fa48fe4
Ludlin1999 <lin.yuchuan0519@gmail.com>
2022-07-10 16:39:07 -0400

    home page hook up with the nav bar

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("main", { title: "Weekend-Escape" });
+  res.render("home/main", { title: "Weekend-Escape" });
 });
 
 module.exports = router;

45b1ccaa5a5d1707306f489541922a18ca251613
Ludlin1999 <lin.yuchuan0519@gmail.com>
2022-07-14 16:19:34 -0400

    final final

--- config/database.js
+++ config/database.js
@@ -2,10 +2,13 @@
 const mongoose = require("mongoose");
 
 //Set up default mongoose connection
-mongoose.connect("mongodb://127.0.0.1:27017/products", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://ludlin886:Ludlin196958@cluster0.afmvot0.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 //Get the default connection
 const db = mongoose.connection;
